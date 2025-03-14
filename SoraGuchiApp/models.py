db008fe6f0abfb24efd2c675869036a61e991365
nakaryunosuke <naka.sava0309@gmail.com>
2025-03-14 11:21:24 +0900

    Add: テーブル名の追記。

--- SoraGuchiApp/models.py
+++ SoraGuchiApp/models.py
@@ -3,4 +3,6 @@ from django.db import models # type: ignore
 class Posts(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=140)
+    class Meta:
+        db_table = "posts"
 

4b9d2f4689b91757bfe72e291ff3cd815fd552ac
nakaryunosuke <naka.sava0309@gmail.com>
2025-03-19 22:54:59 +0900

    Add: Postsテーブルにcreated_at, updated_atを追加。

--- SoraGuchiApp/models.py
+++ SoraGuchiApp/models.py
@@ -1,6 +1,14 @@
 from django.db import models # type: ignore
 
-class Posts(models.Model):
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+class Posts(BaseModel):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=140)
     class Meta:

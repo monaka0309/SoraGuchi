54cc29cff9b556ed282a3d7b88d9fd4e0664a76d
nakaryunosuke <naka.sava0309@gmail.com>
2025-03-26 14:48:10 +0900

    Add: staticの指定方法の修正。

--- SoraGuchiApp/views.py
+++ SoraGuchiApp/views.py
@@ -68,6 +68,7 @@ def post_delete(request, id):
 
 
 def register(request):
+    print(request.POST)
     regist_form = forms.RegistForm(request.POST or None)
     if regist_form.is_valid():
         regist_form.save(commit=True)
